a36ebec52819234e94686eabf895c79bd237b899
Akio Sone <asone@email.unc.edu>
2022-10-05 02:22:23 -0400

    up to each city's line chart

---
+++ demo/2022-10-05/SoGBM2DashBoard/helpers.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+#library(plyr)
+#library(dplyr)
+library(tidyquant)
+library(plotly)
+library(viridis)
+library(leaflet)
+library(DT)
+library(scales)
+library(rjson)
+library(Rmisc)
+bd_data<-read_rds(file="./Benchmarking_2_0_Data_Long_Form.rds")
+m_tbl  <- bd_data %>% distinct(Municipality)
+m_list <- bd_data %>% distinct(Municipality) %>% pull() 
+s_list <- bd_data %>% distinct(Service) %>% filter(Service !="Census") %>% pull()
+s_all_list <- bd_data %>% distinct(Service) %>% pull()
+y_list <- bd_data %>% distinct(Year) %>% pull() 
+citylabel <-c("Apex", "Chapel Hill",   "Charlotte", "Concord", "Goldsboro", "Greensboro", "Hickory", "Raleigh", "Wilson", "Winston-Salem")
+rvllabel <- c("Chapel Hill",   "Charlotte", "Concord", "Goldsboro", "Greensboro", "Hickory", "Raleigh", "Wilson", "Winston-Salem")
+rvllabellist <- setNames(as.list(rvllabel), rvllabel)
+
+v_list <- bd_data %>% filter(Municipality=="Apex") %>% group_by(Service) %>% distinct(Variable)
+
+#s1numlist   <- bd_data %>% filter(Municipality=="Apex") %>% group_by(Service) %>% distinct(Variable) %>% filter(Service == s_list[1] ) %>% pull()
+#s1denomlist <- bd_data %>% filter(Municipality=="Apex") %>% group_by(Service) %>% distinct(Variable) %>% filter(Service == s_list[1] | Service=="Census") %>% pull()
+#s1numlist   <- v_list %>% filter(Service == s_list[1] ) %>% pull()
+#s1denomlist <- v_list %>% filter(Service == s_list[1] | Service=="Census") %>% pull()
+
+service2var <- list()
+for (srv in s_all_list) {
+  service2var[[srv]]  <-bd_data  %>% group_by(Service) %>% distinct(Variable) %>% filter(Service == srv) %>% pull()
+}
+
+
+censusVars <- bd_data  %>% filter(Service=="Census") %>% distinct(Variable) %>% pull()
